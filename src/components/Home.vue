dbae41ffb1f03647ef7586c6355c388aab14f7cc
xxsnake28 <xxsnake28@xxsnake28s-MacBook-Pro.local>
2020-08-04 13:29:30 +0800

    调整一些文件

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">this is my home!</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      pp: 'cccc',
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  text-align: left;
+}
+</style>
